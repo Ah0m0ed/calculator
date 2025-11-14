461384f40264b14a6c0eac180633210443d9e560
Ahmed Medhat <211419359+Ah0m0ed@users.noreply.github.com>
2025-11-14 17:32:03 +0200

    add get prev & curr

---
+++ script.js
@@ -0,0 +1,25 @@
+function getPrev(){
+    let prev = document.querySelector(".prev-op");
+    return parseInt(prev.textContent); 
+}
+
+function getCurr(){
+    let curr = document.querySelector(".curr-op");
+    return parseInt(curr.textContent); 
+}
+
+function add(){
+
+}
+
+function subtract(){
+
+}
+
+function multiply(){
+
+}
+
+function divide(){
+
+}
